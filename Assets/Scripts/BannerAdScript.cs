82e48ed8384408ae6fc14a64a7a5bafb9ffd62ec
Autio <petri.mikael.autio@gmail.com>
2021-05-08 23:50:15 +0300

    Banner ad

--- Assets/Scripts/BannerAdScript.cs
+++ Assets/Scripts/BannerAdScript.cs
@@ -12,12 +12,14 @@ public class BannerAdScript : MonoBehaviour
     void Start()
     {
         Advertisement.Initialize (gameId, testMode);
+        Advertisement.Banner.SetPosition (BannerPosition.TOP_CENTER);
         StartCoroutine (ShowBannerWhenReady ());
     }
     IEnumerator ShowBannerWhenReady () {
         while (!Advertisement.IsReady (placementId)) {
-            yield return new WaitForSeconds (0.5f);
+             yield return new WaitForSeconds (0.5f);
         }
+        yield return new WaitForSeconds (0.1f);
         Advertisement.Banner.Show (placementId);
     }
 
